92107a4b8bb8c80bc4294f240ad9da25b868a4f8
Ahsan Iqbal <revensolutions01@gmail.com>
2024-05-22 21:22:50 +0500

    handle routing issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { Box } from "@mui/material";
 import React from "react";
+import { Box } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  Navbar,
-  Feed,
-  VideoDetail,
-  ChannelDetail,
-  SearchFeed,
-} from "./components";
+import { Navbar, Feed, ChannelDetail, VideoDetail, SearchFeed } from './components';
 
 const App = () => {
   return (
@@ -15,10 +9,10 @@ const App = () => {
       <Box sx={{ background: "#000" }}>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Feed />} />
-          <Route path="/video:id" exact element={<VideoDetail />} />
-          <Route path="/channel:id" exact element={<ChannelDetail />} />
-          <Route path="/Serch:searcTerm" exact element={<SearchFeed />} />
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
         </Routes>
       </Box>
     </BrowserRouter>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <div>VideoDetail by ahsan </div>
   )
 }
 
